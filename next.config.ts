6a4e8ea2e860701560453409471d262e7d516cd1
wenyiqing <wenyiqing@tencent.com>
2025-07-02 10:38:22 +0800

    Config directory of build output

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  // 当执行 `next build` 时，将网站静态导出到 `out` 目录
+  output: 'export',
   images: {
+    // 静态导出模式下禁用默认 Image Optimization，否则 `next build` 会报错
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
